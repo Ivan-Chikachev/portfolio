c7beefce7ff564f6675be902163dcefc4206ef32
Ivan Ch <ivan-chikachev@mail.ru>
2021-03-07 00:26:08 +0300

    add animation

--- script.js
+++ script.js
@@ -2,8 +2,7 @@ window.onscroll = function s() {
     let header = document.querySelector('.header');
     if (window.pageYOffset > 350) {
         header.classList.add('header-fixed');
-    }
-    else {
+    } else {
         header.classList.remove('header-fixed');
     }
 }
@@ -11,9 +10,52 @@ document.querySelector('.btn').onmousemove = (e) => {
 
     const x = e.pageX - e.target.offsetLeft
     const y = e.pageY - e.target.offsetTop
-  
-    e.target.style.setProperty('--x', `${ x }px`)
-    e.target.style.setProperty('--y', `${ y }px`)
-    
-  }
+
+    e.target.style.setProperty('--x', `${x}px`)
+    e.target.style.setProperty('--y', `${y}px`)
+
+}
+
+// -------АНИМАЦИЯ ПРИ СКРОЛЛЕ------ //
+
+const animItems = document.querySelectorAll('._anim');
+
+if (animItems.length > 0) {
+    window.addEventListener('scroll', animOnScroll);
+
+    function animOnScroll() {
+        for (let i = 0; i < animItems.length; i++) {
+            let animItem = animItems[i];
+            let animItemHeight = animItem.offsetHeight;
+            let animItemOffset = offset(animItem).top;
+            let animStart = 5;
+
+            let animItemPoint = window.innerHeight - animItemHeight / animStart;
+            if (animItemHeight > window.innerHeight) {
+                animItemPoint = window.innerHeight - window.innerHeight / animStart;
+            }
+
+            if ((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)) {
+                animItem.classList.add('_active');
+            } else {
+                if (!animItem.classList.contains('_anim-no')) {
+                    animItem.classList.remove('_active');
+                }
+
+            }
+        }
+    }
+
+    function offset(el) {
+        const rect = el.getBoundingClientRect(),
+            scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+            scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+        return {top: rect.top + scrollTop, left: rect.left + scrollLeft}
+    }
+
+    setTimeout(() => {
+        animOnScroll();
+    }, 300)
+}
+
   
